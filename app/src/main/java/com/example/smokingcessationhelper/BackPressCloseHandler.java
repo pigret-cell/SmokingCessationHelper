f976e2203e979f1474f178e21666529ce85bd1e8
jiyoung9811 <66866740+jiyoung9811@users.noreply.github.com>
2020-06-22 19:08:13 +0900

    Add files via upload
    BackPressCloseHandler와 RegisterActivity 수정

--- app/src/main/java/com/example/smokingcessationhelper/BackPressCloseHandler.java
+++ app/src/main/java/com/example/smokingcessationhelper/BackPressCloseHandler.java
@@ -4,30 +4,25 @@ import android.app.Activity;
 import android.widget.Toast;
 
 public class BackPressCloseHandler {
-    private long backKeyPressedTime = 0;
-    private Toast toast;
-
+    private long backKeyClickTime = 0;
     private Activity activity;
 
-    public BackPressCloseHandler(Activity context) {
-        this.activity = context;
+    public BackPressCloseHandler(Activity activity) {
+        this.activity = activity;
     }
 
     public void onBackPressed() {
-        if (System.currentTimeMillis() > backKeyPressedTime + 2000) {
-            backKeyPressedTime = System.currentTimeMillis();
-            showGuide();
+        if (System.currentTimeMillis() > backKeyClickTime + 2000) {
+            backKeyClickTime = System.currentTimeMillis();
+            showToast();
             return;
         }
-        if (System.currentTimeMillis() <= backKeyPressedTime + 2000) {
-            toast.cancel();
-            activity.moveTaskToBack(true);
+        if (System.currentTimeMillis() <= backKeyClickTime + 2000) {
+            activity.finish();
         }
     }
 
-    public void showGuide() {
-        toast = Toast.makeText(activity, "한번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT);
-        toast.show();
+    public void showToast() {
+        Toast.makeText(activity, "뒤로 가기 버튼을 한 번 더 누르면 종료됩니다.", Toast.LENGTH_SHORT).show();
     }
-
 }
--- app/src/main/java/com/example/smokingcessationhelper/RegisterActivity.java
+++ app/src/main/java/com/example/smokingcessationhelper/RegisterActivity.java
@@ -1,14 +1,197 @@
 package com.example.smokingcessationhelper;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.content.CursorLoader;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.storage.StorageManager;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageException;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class RegisterActivity extends AppCompatActivity {
+    private static final String TAG = "RegisterActivity";
+    private FirebaseAuth mAuth;
+    private ImageView imageView;
+    private FirebaseStorage storage;
+    private static final int PICK_IMAGE = 0;
+    private String path, pas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        // Initialize Firebase Auth
+        mAuth = FirebaseAuth.getInstance();
+        storage = FirebaseStorage.getInstance();
+
+        imageView = findViewById(R.id.RegisterActivity_ivSelectImage);
+        imageView.setOnClickListener(onClickListener);
+        findViewById(R.id.RegisterActivity_btRegister).setOnClickListener(onClickListener);
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+    }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch(v.getId()){
+                case R.id.RegisterActivity_btRegister:
+                    signUp();
+                    break;
+                case R.id.RegisterActivity_ivSelectImage:
+                    Intent intent = new Intent(Intent.ACTION_PICK);
+                    intent.setType(MediaStore.Images.Media.CONTENT_TYPE);
+                    startActivityForResult(intent, PICK_IMAGE);
+            }
+        }
+    };
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_IMAGE) {
+           if (resultCode == RESULT_OK) {
+                try {
+                    System.out.println(data.getData());  //변환 전_test
+                    path = getPath(data.getData()); //변환 후
+                    System.out.println(path);
+
+                    Uri pass = data.getData();
+                    pas = pass.toString();
+
+
+                    InputStream in = getContentResolver().openInputStream(data.getData());
+                    Bitmap img = BitmapFactory.decodeStream(in);
+                    in.close();
+                    imageView.setImageBitmap(img);
+                } catch (Exception e) {
+                }
+            }
+            else if(resultCode == RESULT_CANCELED){
+                startToast("선택 취소");
+            }
+        }
+    }
+
+    private String getPath(Uri uri) { //사진 파일경로 변환 코드.
+        String [] pro = {MediaStore.Images.Media.DATA};
+        CursorLoader cursorLoader = new CursorLoader(this, uri, pro,
+                null, null, null);
+        Cursor cursor = cursorLoader.loadInBackground();
+        int index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
+        cursor.moveToFirst();
+        return cursor.getString(index);
+    }
+
+
+    private void signUp() {
+        String email = ((EditText)findViewById(R.id.RegisterActivity_etEmail)).getText().toString();
+        String password = ((EditText)findViewById(R.id.RegisterActivity_etPwd)).getText().toString();
+        String name = ((EditText)findViewById(R.id.RegisterActivity_etId)).getText().toString();
+
+        if(email.length() > 0 && password.length() > 0 && name.length() >0) {
+                mAuth.createUserWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    //FirebaseUser user = mAuth.getCurrentUser();
+                                    startToast("회원가입에 성공했습니다.");
+
+                                    String email = ((EditText)findViewById(R.id.RegisterActivity_etEmail)).getText().toString();
+                                    String password = ((EditText)findViewById(R.id.RegisterActivity_etPwd)).getText().toString();
+                                    String name = ((EditText)findViewById(R.id.RegisterActivity_etId)).getText().toString();
+
+//                                    String mUserEmail = mUser.getEmail();
+                                    FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                                    FirebaseFirestore db = FirebaseFirestore.getInstance();
+                                    UserInfo userInfo = new UserInfo(email, name, password);
+//                                    db.collection(mUserEmail).document("UserInfo").set(userInfo);
+                                    db.collection(email)
+                                            .document("UserInfo")
+                                            .set(userInfo)
+                                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                                @Override
+                                                public void onSuccess(Void aVoid) {
+                                                    Log.d(TAG, "setData:Success ");
+                                                }
+                                            })
+                                            .addOnFailureListener(new OnFailureListener() {
+                                                @Override
+                                                public void onFailure(@NonNull Exception e) {
+                                                    Log.w(TAG, "Error setting document", e);
+                                                }
+                                            });
+
+
+                                    if(path != null) {
+                                        StorageReference storageRef = storage.getReference();
+                                        Uri file = Uri.fromFile(new File(path));
+                                        StorageReference profile = storageRef.child("images/" + email);
+                                        UploadTask uploadTask = profile.putFile(file);
+                                        uploadTask
+                                                .addOnFailureListener(new OnFailureListener() {
+                                                    @Override
+                                                    public void onFailure(@NonNull Exception exception) {
+                                                        startToast("에러");
+                                                        int errorCode = ((StorageException) exception).getErrorCode();
+                                                        String errorMessage = exception.getMessage();
+                                                    }
+                                                })
+                                                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                                                    @Override
+                                                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                                                    startToast("성공");
+                                                    }
+                                                });
+                                    }
+                                }
+                                else {startToast("회원가입에 실패했습니다."); }
+                            }
+                        });
+        }else {
+            startToast("이름과 이메일, 비밀번호를 입력해주세요.");
+        }
+    }
+
+
+    private void startToast(String msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
 }
